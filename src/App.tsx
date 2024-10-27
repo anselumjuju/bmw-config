b7257b0707d89c7ea5bc5b1024080c59cb2824e8
juju-anselum <anselumjuju@gmail.com>
2024-10-27 15:07:01 +0530

    Add cyberpunk env

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { Canvas } from '@react-three/fiber';
-import { Experience } from './components';
+import { Environment, OrbitControls, Grid, AdaptiveDpr } from '@react-three/drei';
 import './App.css';
+import { Experience } from './components';
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing';
 
-function App() {
+export function App() {
   return (
-    <div className='w-full h-screen overflow-hidden bg-black'>
-      <Canvas>
-        <color attach='background' args={['#111115']} />
-        <Experience />
+    <div className='w-full h-dvh'>
+      <Canvas flat shadows camera={{ position: [12, 5, 10], fov: 25 }}>
+        <fog attach="fog" args={['black', 15, 30]} />
+        <group castShadow={true}>
+          <Experience />
+        </group>
+        <Grid renderOrder={-1} position={[0, 0, 0]} receiveShadow infiniteGrid cellSize={0.8} cellThickness={0.6} sectionSize={2} sectionThickness={2} sectionColor={'#f29ffb'} fadeDistance={26} />
+        <OrbitControls autoRotate autoRotateSpeed={-0.075} enableZoom={false} makeDefault minPolarAngle={Math.PI / 2.075} maxPolarAngle={Math.PI / 2.075} />
+        <EffectComposer>
+          <Bloom luminanceThreshold={0.4} intensity={0.6} />
+          <ToneMapping />
+        </EffectComposer>
+        <Environment background preset="sunset" backgroundBlurriness={0.7} backgroundIntensity={0.4} />
+        <AdaptiveDpr pixelated />
       </Canvas>
     </div>
   );
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,15 +1,6 @@
-import { CameraControls } from '@react-three/drei';
-import M4 from './M4';
+import { M4 } from './M4';
 
-const Experience = () => {
-  return (
-    <>
-      <pointLight position={[0, 2, 2]} intensity={10} />
-      <ambientLight intensity={0.5} />
-      <CameraControls />
-      <M4 />
-    </>
-  );
-};
+const Experience = () => <M4 />
+
 
 export default Experience;
