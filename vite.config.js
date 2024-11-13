22e29835c6603c72ec1d34a851dcf3fa6a60ba2a
juju-anselum <anselumjuju@gmail.com>
2024-11-13 17:48:32 +0530

    OpenGraph added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.180.105',
+    host: '192.168.29.180',
     port: 3000
   }
 })
